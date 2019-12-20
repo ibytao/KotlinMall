68539545688aac6658fc0299f93f260554756b3e
Buck <bytao@icloud.com>
2019-12-20 19:08:39 +0800

    up

--- userCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
+++ userCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
@@ -1,20 +1,28 @@
 package com.kotlin.user.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import android.widget.Toast
+import com.kotlin.base.ui.activity.BaseMvpActivity
 import com.kotlin.user.R
+import com.kotlin.user.presenter.RegisterPresenter
+import com.kotlin.user.presenter.view.RegisterView
 import kotlinx.android.synthetic.main.activity_register.*
 import org.jetbrains.anko.toast
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity: BaseMvpActivity<RegisterPresenter>(), RegisterView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        mPresenter = RegisterPresenter()
+        mPresenter.mView = this
+
         mRegisterBtn.setOnClickListener {
-            toast("ok")
+            mPresenter.register("", "")
         }
+    }
+
+    override fun onRegisterResult(result: Boolean) {
+        toast("ok")
     }
 }
